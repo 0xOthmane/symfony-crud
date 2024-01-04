30a29451087f5c592e0e2333d6be07f5b42c55ce
0xOthmane <0xothodor@proton.me>
2024-01-04 16:21:31 +0100

    feat: add RecipeController and views

---
+++ src/Controller/RecipeController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\RecipeRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RecipeController extends AbstractController
+{
+    /**
+     * List all recipes
+     *
+     * @param RecipeRepository $recipeRepository
+     * @param PaginatorInterface $paginator
+     * @param Request $request
+     * @return Response
+     */
+    #[Route('/recette', name: 'app_recipe', methods: ['GET'])]
+    public function index(RecipeRepository $recipeRepository, PaginatorInterface $paginator, Request $request): Response
+    {
+        $recipes = $paginator->paginate(
+            $recipeRepository->findAll(),
+            $request->query->getInt('page', 1)
+        );
+        return $this->render('pages/recipe/index.html.twig', [
+            'recipes' => $recipes,
+        ]);
+    }
+}
